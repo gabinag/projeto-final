4af60ede63c28e97d2cb7f63ab91f223495ff97c
Gabriela Nagamuta <gnagamuta03@gmail.com>
2022-11-19 19:17:26 -0300

    Atualizando CSS

---
+++ main.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems);
+});
+
+
+
+function mudaTema() {
+  document.body.classList.toggle("dark");
+  
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
