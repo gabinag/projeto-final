882c3b46508d2e5fc7e44a601617b8eb72826fc3
Gabriela Nagamuta <gnagamuta03@gmail.com>
2022-11-21 22:39:30 -0300

    Adicionando function da galeria

--- main.js
+++ main.js
@@ -1,19 +1,43 @@
+//Carrossel
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.carousel');
   var instances = M.Carousel.init(elems);
 });
 
 
-
+//Altera cores do tema
 function mudaTema() {
   document.body.classList.toggle("dark");
   
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.materialboxed');
-  var instances = M.Materialbox.init(elems);
+
+//Galeria
+const lista = [];
+function addImage() {
+   const inputFile = document.querySelector("#file");
+   const div = document.getElementById("container");
+  
+  inputFile.addEventListener("change", function (e) {
+  const inputTarget = e.target;
+  let file = inputTarget.files[0];
+
+  const reader = new FileReader();
+
+  reader.addEventListener("load", function (e) {
+  const readerTarget = e.target;
+  
+  const img = document.createElement("img");
+
+  img.src = readerTarget.result;
+  img.classList.add("picture");
+  
+  div.appendChild(img);
+  });
+    
+  reader.readAsDataURL(file);
 });
+}
 
 
 
